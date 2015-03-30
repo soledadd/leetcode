7b3865d51471a22786a8e9a9362dcb4752346170
soldadd <825415493@qq.com>
2015-03-30 21:45:25 +0800

    Compare Version Numbers

--- Compare Version Numbers.cpp
+++ Compare Version Numbers.cpp
@@ -1,33 +1,82 @@
-int compareVersion(string version1, string version2) {
-  int l1 = version1.length();
-  int l2 = version2.length();
-  float res1,res2;
-  res1 = res2 = 0;
-  for (int i = 0; i < l1;i++) {
-    if (version1[i] != '.') {
-      res1 = res1 * 10 + version1[i] - '0';
-    } else {   
-      float tmp = 1.0;   
-      for (++i;i < l1;i++) {
-        tmp *= 0.1
-        res1 += version1[i] * tmp;
-      }
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        vector<int> vervec1;
+        vector<int> vervec2;
+
+        splitVersionString(version1, vervec1);
+        splitVersionString(version2, vervec2);
+
+        int maxI = max(vervec1.size(), vervec2.size());
+        int i;
+        for (i=0; i<maxI; ++i)
+        {
+            int v1 = i<vervec1.size() ? vervec1[i] : 0;
+            int v2 = i<vervec2.size() ? vervec2[i] : 0;
+
+            if (v1 < v2)
+            {
+                return -1;
+            }
+            else if (v1 > v2)
+            {
+                return 1;
+            }
+        }
+
+        return 0;
     }
-  }
-  for (int i = 0; i < l2;i++) {
-    if (version2[i] != '.') {
-      res2 = res2 * 10 + version2[i] - '0';
-    } else {   
-      float tmp = 1.0;   
-      for (++i;i < l2;i++) {
-        tmp *= 0.1
-        res2 += version2[i] * tmp;
-      }
+
+private:
+    static void splitVersionString(string version,vector<int>& vervec)
+    {
+        int tmp = 0;
+        for(size_t i=0; i<version.size(); ++i)
+        {
+            if (version[i] == '0' && tmp == 0)
+            {
+                continue;
+            }
+            else if (version[i] != '.')
+            {
+                tmp = tmp * 10 + atoi(&version[i]);
+            }
+            else
+            {
+                vervec.push_back(tmp);
+                tmp = 0;
+            }
+        }
+        vervec.push_back(tmp);
     }
-  }
-  if (res1 == res2) {
-    return 0;
-  } else {
-    return res1 > res2?1:-1;
-  }
-}
+};
+
+
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        long res1=0, res2 = 0;
+        unsigned int i=0, j=0;
+        while (i < version1.size() || j < version2.size()){
+            for (; i < version1.size(); i++)
+                    if (isdigit(version1[i]))
+                        res1 = res1 * 10 + version1[i] - '0';
+                    else
+                        break;
+
+            for (; j < version2.size(); j++)
+                    if (isdigit(version2[j]))
+                       res2 = res2 * 10 + version2[j] - '0';
+                   else
+                       break;
+
+            if (res1 > res2)      return  1;
+            else if (res1 < res2) return -1;
+            i++;
+            j++;
+            res1 = 0;
+            res2 = 0;
+        }
+        return 0;
+    }
+};
